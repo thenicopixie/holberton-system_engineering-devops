658e8fc06df85fdceca5354b0d7e42bd988c44fe
thenicopixie <thenicopixie>
2019-02-11 22:00:34 +0000

    script that returns information about user's TODO list progress

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Script that returns information about a user's TODO list progress"""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId=' +
+                         sys.argv[1]).json()
+    user = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                        sys.argv[1]).json()
+    username = user.get('name')
+    completed = [c.get('title') for c in todos if c.get('completed')]
+    print("Employee {} is done with tasks({}/{}):".format(
+          username, len(completed), len(completed)), end="")
+    for task in completed:
+        print("\n {}".format(task), end="")
+    print("")
