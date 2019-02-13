2bfc0f5135e32f1607ade2f939a6e30b29e86bcf
thenicopixie <thenicopixie>
2019-02-13 21:41:22 +0000

    add check for none

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,10 +8,13 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return number of subscribers of a subreddit"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = "https://api.reddit.com/r/{}/about".format(subreddit)
     try:
-        return requests.get(url, allow_redirects=False,
+        ret = requests.get(url, allow_redirects=False,
                             headers={'User-Agent': 'user'}
                             ).json().get("data").get("subscribers")
+        if ret is None:
+            return 0
+        return ret
     except:
         return 0
